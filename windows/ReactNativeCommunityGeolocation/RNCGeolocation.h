5b5734a62e845b45b3ff6955d11bd118c754db0b
Bryan Jeal <bjeal@iti4dmv.com>
2023-11-09 11:19:29 -0700

    Windows Implementation - Made the Location Reporting Interval configurable.

--- windows/ReactNativeCommunityGeolocation/RNCGeolocation.h
+++ windows/ReactNativeCommunityGeolocation/RNCGeolocation.h
@@ -2,18 +2,18 @@
 #pragma once
 
 #include "pch.h"
+#include <chrono>
+#include <cmath>
 #include <functional>
 #include <sstream>
 #include <string>
-#include <cmath>
-#include <chrono>
 
+#include "GeoConfiguration.h"
+#include "GeoOptions.h"
+#include "GeolocationCoordinates.h"
+#include "GeolocationPosition.h"
 #include "JSValue.h"
 #include "NativeModules.h"
-#include "GeoOptions.h"
-#include "GeoConfiguration.h"
-#include "GeolocationPosition.h"
-#include "GeolocationCoordinates.h"
 
 using namespace winrt;
 using namespace Windows::Devices::Geolocation;
@@ -22,202 +22,202 @@ using namespace Windows::UI::Core;
 using namespace Microsoft::ReactNative;
 using namespace std::chrono;
 
-namespace winrt::ReactNativeCommunityGeolocation
-{
-    REACT_MODULE(RNCGeolocation);
-    struct RNCGeolocation
-    {
-        Geolocator _locator;
-        GeoConfiguration _config;
+namespace winrt::ReactNativeCommunityGeolocation {
+REACT_MODULE(RNCGeolocation);
+struct RNCGeolocation {
+	Geolocator _locator;
+	GeoConfiguration _config;
 
-        REACT_METHOD(addListener, L"addListener")
-        void addListener(std::string eventName) noexcept {
-            // Keep: Required for RN built in Event Emitter Calls.
-        }
+	REACT_METHOD(addListener, L"addListener")
+	void addListener(std::string eventName) noexcept {
+		// Keep: Required for RN built in Event Emitter Calls.
+	}
 
-        REACT_METHOD(removeListeners, L"removeListeners")
-        void removeListeners(int count) noexcept {
-            // Keep: Required for RN built in Event Emitter Calls.
-        }
+	REACT_METHOD(removeListeners, L"removeListeners")
+	void removeListeners(int count) noexcept {
+		// Keep: Required for RN built in Event Emitter Calls.
+	}
 
-        REACT_METHOD(SetConfiguration, L"setConfiguration");
-        void SetConfiguration(GeoConfiguration config) noexcept
-        {
-            _config = config;
-        }
+	REACT_METHOD(SetConfiguration, L"setConfiguration");
+	void SetConfiguration(GeoConfiguration config) noexcept {
+		_config = config;
+	}
 
-        REACT_METHOD(RequestAuthorization, L"requestAuthorization");
-        void RequestAuthorization(ReactPromise<void> promise) noexcept
-        {
-            auto asyncOp = RequestAuthorizationAsync(promise);
-            asyncOp.Completed(MakeAsyncActionCompletedHandler(promise));
-        }
+	REACT_METHOD(RequestAuthorization, L"requestAuthorization");
+	void RequestAuthorization(ReactPromise<void> promise) noexcept {
+		auto asyncOp = RequestAuthorizationAsync(promise);
+		asyncOp.Completed(MakeAsyncActionCompletedHandler(promise));
+	}
 
-        static IAsyncAction RequestAuthorizationAsync(ReactPromise<void> promise) noexcept
-        {
-            auto capturedPromise = promise;
-            auto access = co_await Geolocator::RequestAccessAsync();
-            if (access == GeolocationAccessStatus::Allowed) {
-                capturedPromise.Resolve();
-            }
-            else if (access == GeolocationAccessStatus::Denied) {
-                capturedPromise.Reject("Access was denied.");
-            }
-            else {
-                capturedPromise.Reject("Access was unknown.");
-            }
-        }
+	static IAsyncAction
+	RequestAuthorizationAsync(ReactPromise<void> promise) noexcept {
+		auto capturedPromise = promise;
+		auto access          = co_await Geolocator::RequestAccessAsync();
+		if (access == GeolocationAccessStatus::Allowed) {
+			capturedPromise.Resolve();
+		} else if (access == GeolocationAccessStatus::Denied) {
+			capturedPromise.Reject("Access was denied.");
+		} else {
+			capturedPromise.Reject("Access was unknown.");
+		}
+	}
 
+	REACT_METHOD(GetCurrentPosition, L"getCurrentPosition");
+	void GetCurrentPosition(GeoOptions options,
+	                        ReactPromise<JSValueObject> promise) noexcept {
+		auto asyncOp = GetCurrentPositionAsync(options, promise);
+		asyncOp.Completed(MakeAsyncActionCompletedHandler(promise));
+	}
 
-        REACT_METHOD(GetCurrentPosition, L"getCurrentPosition");
-        void GetCurrentPosition(GeoOptions options, ReactPromise<JSValueObject> promise) noexcept
-        {
-            auto asyncOp = GetCurrentPositionAsync(options, promise);
-            asyncOp.Completed(MakeAsyncActionCompletedHandler(promise));
-        }
+	static IAsyncAction
+	GetCurrentPositionAsync(GeoOptions options,
+	                        ReactPromise<JSValueObject> promise) noexcept {
+		auto capturedPromise = promise;
+		auto locator         = CreateGeolocator(options);
 
-        static IAsyncAction GetCurrentPositionAsync(GeoOptions options, ReactPromise<JSValueObject> promise) noexcept
-        {
-            auto capturedPromise = promise;
-            auto locator = CreateGeolocator(options);
+		auto position     = co_await locator.GetGeopositionAsync();
+		auto resultObject = ToJSValueObject(position.Coordinate());
 
-            auto position = co_await locator.GetGeopositionAsync();
-            auto resultObject = ToJSValueObject(position.Coordinate());
+		capturedPromise.Resolve(resultObject);
+	}
 
-            capturedPromise.Resolve(resultObject);
-        }
+	winrt::event_token _positionChangedEventToken;
 
+	REACT_METHOD(StartObserving, L"startObserving");
+	void StartObserving(GeoOptions options) noexcept {
+		EnsureGeolocatorInitialized(options);
 
-        winrt::event_token _positionChangedEventToken;
+		_positionChangedEventToken = _locator.PositionChanged(
+		    {this, &RNCGeolocation::OnPositionChanged});
+	}
 
-        REACT_METHOD(StartObserving, L"startObserving");
-        void StartObserving(GeoOptions options) noexcept
-        {
-            EnsureGeolocatorInitialized(options);
+	void OnPositionChanged(Geolocator const &,
+	                       PositionChangedEventArgs const &e) noexcept {
+		auto result = ToJSValueObject(e.Position().Coordinate());
+		GeolocationDidChange(result);
+	}
 
-            _positionChangedEventToken = _locator.PositionChanged({ this, &RNCGeolocation::OnPositionChanged });
-        }
+	REACT_METHOD(StopObserving, L"stopObserving");
+	void StopObserving() noexcept {
+		if (_locator != nullptr) {
+			_locator.PositionChanged(_positionChangedEventToken);
+		}
+	}
 
-        void OnPositionChanged(Geolocator const&, PositionChangedEventArgs const& e) noexcept {
-            auto result = ToJSValueObject(e.Position().Coordinate());
-            GeolocationDidChange(result);
-        }
+	REACT_EVENT(GeolocationDidChange, L"geolocationDidChange");
+	std::function<void(JSValueObject &)> GeolocationDidChange;
 
-        REACT_METHOD(StopObserving, L"stopObserving");
-        void StopObserving() noexcept
-        {
-            if (_locator != nullptr) {
-                _locator.PositionChanged(_positionChangedEventToken);
-            }
-        }
+	REACT_METHOD(GetStatus, L"getStatus");
+	std::string GetStatus(GeoOptions options) noexcept {
+		EnsureGeolocatorInitialized(options);
 
-        REACT_EVENT(GeolocationDidChange, L"geolocationDidChange");
-        std::function<void(JSValueObject&)> GeolocationDidChange;
+		return StatusToString(_locator.LocationStatus());
+	}
 
+	winrt::event_token _statusChangedEventToken;
 
-        REACT_METHOD(GetStatus, L"getStatus");
-        std::string GetStatus(GeoOptions options) noexcept
-        {
-            EnsureGeolocatorInitialized(options);
+	REACT_METHOD(StartObservingStatus, L"startObservingStatus");
+	void StartObservingStatus(GeoOptions options) noexcept {
+		EnsureGeolocatorInitialized(options);
 
-            return StatusToString(_locator.LocationStatus());
-        }
+		_statusChangedEventToken =
+		    _locator.StatusChanged({this, &RNCGeolocation::OnStatusChanged});
+	}
 
-        winrt::event_token _statusChangedEventToken;
+	void OnStatusChanged(Geolocator const &,
+	                     StatusChangedEventArgs const &e) noexcept {
+		StatusDidChange(StatusToString(e.Status()));
+	}
 
-        REACT_METHOD(StartObservingStatus, L"startObservingStatus");
-        void StartObservingStatus(GeoOptions options) noexcept
-        {
-            EnsureGeolocatorInitialized(options);
+	REACT_METHOD(StopObservingStatus, L"stopObservingStatus");
+	void StopObservingStatus() noexcept {
+		if (_locator != nullptr) {
+			_locator.StatusChanged(_statusChangedEventToken);
+		}
+	}
 
-            _statusChangedEventToken = _locator.StatusChanged({ this, &RNCGeolocation::OnStatusChanged });
-        }
+	REACT_EVENT(StatusDidChange, L"statusDidChange");
+	std::function<void(std::string)> StatusDidChange;
 
-        void OnStatusChanged(Geolocator const&, StatusChangedEventArgs const& e) noexcept {
-            StatusDidChange(StatusToString(e.Status()));
-        }
+	std::string StatusToString(PositionStatus status) {
+		switch (status) {
+		case PositionStatus::Initializing:
+			return "initializing";
+		case PositionStatus::Disabled:
+			return "disabled";
+		case PositionStatus::NoData:
+			return "no-data";
+		case PositionStatus::NotAvailable:
+			return "not-available";
+		case PositionStatus::NotInitialized:
+			return "not-initialized";
+		case PositionStatus::Ready:
+			return "ready";
+		default:
+			return "unknown";
+		}
+	}
 
-        REACT_METHOD(StopObservingStatus, L"stopObservingStatus");
-        void StopObservingStatus() noexcept
-        {
-            if (_locator != nullptr) {
-                _locator.StatusChanged(_statusChangedEventToken);
-            }
-        }
+	void EnsureGeolocatorInitialized(GeoOptions options) {
+		if (_locator == nullptr) {
+			_locator = CreateGeolocator(options);
+		}
+	}
 
-        REACT_EVENT(StatusDidChange, L"statusDidChange");
-        std::function<void(std::string)> StatusDidChange;
+	static Geolocator CreateGeolocator(GeoOptions options) {
+		auto locator            = Geolocator();
+		uint32_t maxAge         = std::abs(std::round(options.MaximumAge));
+		uint32_t reportInterval = std::max(maxAge, (uint32_t)1000);
+		locator.ReportInterval(reportInterval);
+		if (options.EnableHighAccuracy) {
+			locator.DesiredAccuracy(PositionAccuracy::High);
+		}
+		return locator;
+	}
 
-        std::string StatusToString(PositionStatus status) {
-            switch (status)
-            {
-            case PositionStatus::Initializing: return "initializing";
-            case PositionStatus::Disabled: return "disabled";
-            case PositionStatus::NoData: return "no-data";
-            case PositionStatus::NotAvailable: return "not-available";
-            case PositionStatus::NotInitialized: return "not-initialized";
-            case PositionStatus::Ready: return "ready";
-            default: return "unknown";
-            }
-        }
+	static JSValueObject
+	ToJSValueObject(winrt::Windows::Devices::Geolocation::Geocoordinate coord) {
 
-        void EnsureGeolocatorInitialized(GeoOptions options)
-        {
-            if (_locator == nullptr) {
-                _locator = CreateGeolocator(options);
-            }
-        }
+		auto unixtime = winrt::clock::to_time_t(coord.Timestamp());
 
-        static Geolocator CreateGeolocator(GeoOptions options) {
-            auto locator = Geolocator();
-            locator.ReportInterval(1000);
-            if (options.EnableHighAccuracy) {
-                locator.DesiredAccuracy(PositionAccuracy::High);
-            }
-            return locator;
-        }
+		auto result = JSValueObject{
+		    {"coords",
+		     JSValueObject{{"latitude", coord.Latitude()},
+		                   {"longitude", coord.Longitude()},
+		                   {"accuracy", coord.Accuracy()},
+		                   {"heading", ifFinite(coord.Heading(), 0.0)},
+		                   {"speed", ifFinite(coord.Speed(), 0.0)}}},
+		    {"timestamp", unixtime}};
 
-        static JSValueObject ToJSValueObject(winrt::Windows::Devices::Geolocation::Geocoordinate coord) {
+		return result;
+	}
 
-            auto unixtime = winrt::clock::to_time_t(coord.Timestamp());
+	static double ifFinite(IReference<double> reference, double defaultValue) {
+		double value = reference.GetDouble();
+		if (std::isfinite(value)) {
+			return value;
+		}
+		return defaultValue;
+	}
 
-            auto result = JSValueObject{
-                { "coords", JSValueObject {
-                    { "latitude", coord.Latitude() },
-                    { "longitude", coord.Longitude() },
-                    { "accuracy", coord.Accuracy() },
-                    { "heading", ifFinite(coord.Heading(), 0.0) },
-                    { "speed", ifFinite(coord.Speed(), 0.0) }
-                    }
-                },
-                { "timestamp", unixtime }
-            };
+	template <class TPromiseResult>
+	winrt::AsyncActionCompletedHandler MakeAsyncActionCompletedHandler(
+	    winrt::Microsoft::ReactNative::ReactPromise<TPromiseResult> const
+	        &promise) {
+		return
+		    [promise](winrt::IAsyncAction action, winrt::AsyncStatus status) {
+			    if (status == winrt::AsyncStatus::Error) {
+				    std::stringstream errorCode;
+				    errorCode << "0x" << std::hex << action.ErrorCode()
+				              << std::endl;
 
-            return result;
-        }
-
-        static double ifFinite(IReference<double> reference, double defaultValue) {
-            double value = reference.GetDouble();
-            if (std::isfinite(value)) {
-                return value;
-            }
-            return defaultValue;
-        }
-
-        template <class TPromiseResult>
-        winrt::AsyncActionCompletedHandler MakeAsyncActionCompletedHandler(
-            winrt::Microsoft::ReactNative::ReactPromise<TPromiseResult> const& promise) {
-            return [promise](winrt::IAsyncAction action, winrt::AsyncStatus status) {
-                if (status == winrt::AsyncStatus::Error) {
-                    std::stringstream errorCode;
-                    errorCode << "0x" << std::hex << action.ErrorCode() << std::endl;
-
-                    auto error = winrt::Microsoft::ReactNative::ReactError();
-                    error.Message = "HRESULT " + errorCode.str() + ": " + std::system_category().message(action.ErrorCode());
-                    promise.Reject(error);
-                }
-            };
-        }
-
-    };
-}
+				    auto error = winrt::Microsoft::ReactNative::ReactError();
+				    error.Message =
+				        "HRESULT " + errorCode.str() + ": " +
+				        std::system_category().message(action.ErrorCode());
+				    promise.Reject(error);
+			    }
+		    };
+	}
+};
+} // namespace winrt::ReactNativeCommunityGeolocation
